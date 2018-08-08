307dc9b00036977552139e0df6c7521ed8caa155
hamstar0 <webutek@gmail.com>
2018-08-08 04:49:29 -0700

    v2.0.5 * Fixed occasional draw error

--- StaminaMod_Draw.cs
+++ StaminaMod_Draw.cs
@@ -10,7 +10,7 @@ using HamstarHelpers.Helpers.DebugHelpers;
 
 namespace Stamina {
 	partial class StaminaMod : Mod {
-		private bool StaminaBarDraw() {
+		private bool StaminaBarUIDraw() {
 			Player player = Main.LocalPlayer;
 			StaminaPlayer myplayer = player.GetModPlayer<StaminaPlayer>();
 			if( !myplayer.HasEnteredWorld ) { return true; }
@@ -45,6 +45,37 @@ namespace Stamina {
 		}
 
 
+		private bool StaminaBarPlayerDraw() {
+			Player player = Main.LocalPlayer;
+			StaminaPlayer modplayer = player.GetModPlayer<StaminaPlayer>();
+			if( !modplayer.HasEnteredWorld ) { return true; }
+
+			SpriteBatch sb = Main.spriteBatch;
+			if( sb == null ) { return true; }
+
+			try {
+				float alpha = modplayer.Logic.DrainingFX ? 1f : 0.65f;
+				int stamina = (int)modplayer.Logic.Stamina;
+				int max_stamina = modplayer.Logic.MaxStamina2;
+				float fatigue = modplayer.Logic.Fatigue;
+				bool is_exercising = modplayer.Logic.IsExercising;
+				int threshold = fatigue > 0 ? modplayer.Logic.GetStaminaLossAmountNeededForExercise( this ) : -1;
+
+				if( this.Config.ShowMiniStaminaBar ) {
+					int plr_x = (int)( player.position.X - Main.screenPosition.X ) + ( player.width / 2 );
+					int plr_y = (int)( player.position.Y - Main.screenPosition.Y ) + player.height;
+					plr_x += this.Config.PlayerStaminaBarOffsetX;
+					plr_y += this.Config.PlayerStaminaBarOffsetY;
+
+					StaminaUI.DrawShortStaminaBar( sb, plr_x, plr_y, stamina, max_stamina, (int)fatigue, threshold, is_exercising, alpha, 1f );
+				}
+			} catch( Exception e ) { ErrorLogger.Log( e.ToString() ); }
+			return true;
+		}
+
+
+		////////////////
+
 		public override void ModifyInterfaceLayers( List<GameInterfaceLayer> layers ) {
 			if( !this.Config.Enabled ) { return; }
 
@@ -52,39 +83,12 @@ namespace Stamina {
 			if( idx == -1 ) { return; }
 
 			if( this.Config.ShowMainStaminaBar ) {
-				var main_ui_layer = new LegacyGameInterfaceLayer( "Stamina: Main Meter", this.StaminaBarDraw, InterfaceScaleType.UI );
+				var main_ui_layer = new LegacyGameInterfaceLayer( "Stamina: Main Meter", this.StaminaBarUIDraw, InterfaceScaleType.UI );
 				layers.Insert( idx + 1, main_ui_layer );
 			}
 
 			if( this.Config.ShowMiniStaminaBar ) {
-				GameInterfaceDrawMethod func2 = delegate {
-					Player player = Main.LocalPlayer;
-					StaminaPlayer modplayer = player.GetModPlayer<StaminaPlayer>();
-					if( !modplayer.HasEnteredWorld ) { return true; }
-
-					SpriteBatch sb = Main.spriteBatch;
-
-					try {
-						float alpha = modplayer.Logic.DrainingFX ? 1f : 0.65f;
-						int stamina = (int)modplayer.Logic.Stamina;
-						int max_stamina = modplayer.Logic.MaxStamina2;
-						float fatigue = modplayer.Logic.Fatigue;
-						bool is_exercising = modplayer.Logic.IsExercising;
-						int threshold = fatigue > 0 ? modplayer.Logic.GetStaminaLossAmountNeededForExercise( this ) : -1;
-
-						if( this.Config.ShowMiniStaminaBar ) {
-							int plr_x = (int)( player.position.X - Main.screenPosition.X ) + ( player.width / 2 );
-							int plr_y = (int)( player.position.Y - Main.screenPosition.Y ) + player.height;
-							plr_x += this.Config.PlayerStaminaBarOffsetX;
-							plr_y += this.Config.PlayerStaminaBarOffsetY;
-
-							StaminaUI.DrawShortStaminaBar( sb, plr_x, plr_y, stamina, max_stamina, (int)fatigue, threshold, is_exercising, alpha, 1f );
-						}
-					} catch( Exception e ) { ErrorLogger.Log( e.ToString() ); }
-					return true;
-				};
-
-				var plr_ui_layer = new LegacyGameInterfaceLayer( "Stamina: Player Meter", func2, InterfaceScaleType.Game );
+				var plr_ui_layer = new LegacyGameInterfaceLayer( "Stamina: Player Meter", this.StaminaBarPlayerDraw, InterfaceScaleType.Game );
 				layers.Insert( idx + 1, plr_ui_layer );
 			}
 		}
